738d335f9c9ee9825deb9b6892fb1727da0ce823
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-09-28 13:39:35 +0300

    Changed default profile title to name

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
         static::created(
             function($user) {
                 $user->profile()->create([
-                    'title' => $user->username,
+                    'title' => $user->name,
                 ]);
             }
         );
